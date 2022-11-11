2a5d35895b4371fa491f5251c174de0b38bcb835
Molashvili15 <114745456+Molashvili15@users.noreply.github.com>
2022-11-11 12:56:33 +0400

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let userName = "nika";
+let userAge = 15;
+let isStudent = true;
+let myNull = null;
+let myNan = NaN;
+let myUndefined;
+
+console.log(typeof userName, typeof userAge, typeof isStudent, typeof myNull, typeof myNan, typeof myUndefined);
+
+let myName = "nikolozi";
+let myHobby = "coding";
+let yearOfBirth = 2007;
+let currentYear = 2022;
+
+console.log(typeof myName, typeof myHobby, typeof yearOfBirth, typeof currentYear);
+const every =`my name is ${myName}, I am ${currentYear-yearOfBirth} old and my hobby is ${myHobby}.`;
+console.log(every);
+
+
